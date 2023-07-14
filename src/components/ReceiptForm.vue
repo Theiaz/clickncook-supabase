fec9b29fdc1d47f8af108f23c9c037527ed116ff
Julian Schaefer <schaefer@synyx.de>
2023-07-14 12:24:04 +0200

    chore: initial commit

---
+++ src/components/ReceiptForm.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const name = ref<string>('')
+const description = ref<string>('')
+var loading = ref<boolean>(false)
+
+const createReceipt = () => {}
+</script>
+<template>
+  <form @submit.prevent="createReceipt">
+    <div>
+      <label for="name">Name</label>
+      <input id="name" type="text" v-model="name" />
+    </div>
+    <div>
+      <label for="description">Description</label>
+      <textarea id="description" type="text" v-model="description" />
+    </div>
+    <div>
+      <input type="submit" :value="loading ? 'Loading ...' : 'Update'" :disabled="loading" />
+    </div>
+  </form>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ReceiptForm from '@/components/ReceiptForm.vue'
+</script>
+
+<template>
+  <main>
+    <ReceiptForm />
+  </main>
+</template>
