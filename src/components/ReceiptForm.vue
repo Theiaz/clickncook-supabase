2b707809d9d9da819346e6debddae56bca57fbe9
Julian Schaefer <schaefer@synyx.de>
2023-07-14 14:22:51 +0200

    feat: create and update receipt

--- src/components/ReceiptForm.vue
+++ src/components/ReceiptForm.vue
@@ -1,11 +1,58 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import { supabase } from '../supabase'
 
 const name = ref<string>('')
 const description = ref<string>('')
 var loading = ref<boolean>(false)
 
-const createReceipt = () => {}
+onMounted(() => {
+  getReceipt()
+})
+
+const createReceipt = async () => {
+  try {
+    loading.value = true
+    // const { user } = session.value
+
+    const updates = {
+      id: 1,
+      name: name.value,
+      description: description.value
+    }
+
+    let { error } = await supabase.from('Receipt').upsert(updates)
+
+    if (error) throw error
+  } catch (error) {
+    alert(error.message)
+  } finally {
+    loading.value = false
+  }
+}
+
+const getReceipt = async () => {
+  try {
+    loading.value = true
+    //const { user } = session.value
+
+    let { data, error, status } = await supabase
+      .from('Receipt')
+      .select(`name, description`)
+      .single()
+
+    if (error && status !== 406) throw error
+
+    if (data) {
+      name.value = data.name
+      description.value = data.description
+    }
+  } catch (error) {
+    alert(error.message)
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 <template>
   <form @submit.prevent="createReceipt">
