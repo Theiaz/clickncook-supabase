fd1f1e5b1c65c1053553637df0b14e9faf5479c6
Julian Schaefer <schaefer@synyx.de>
2023-07-15 15:42:30 +0200

    chore: use local development for supabase

---
+++ supabase/migrations/20230715133817_remote_commit.sql
@@ -0,0 +1,123 @@
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
+
+CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
+
+CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
+
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+CREATE TABLE "public"."Receipt" (
+    "id" integer NOT NULL,
+    "name" "text" NOT NULL,
+    "description" "text",
+    "authorId" integer
+);
+
+ALTER TABLE "public"."Receipt" OWNER TO "postgres";
+
+CREATE SEQUENCE "public"."Receipt_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE "public"."Receipt_id_seq" OWNER TO "postgres";
+
+ALTER SEQUENCE "public"."Receipt_id_seq" OWNED BY "public"."Receipt"."id";
+
+CREATE TABLE "public"."User" (
+    "id" integer NOT NULL,
+    "email" "text" NOT NULL,
+    "name" "text"
+);
+
+ALTER TABLE "public"."User" OWNER TO "postgres";
+
+CREATE SEQUENCE "public"."User_id_seq"
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE "public"."User_id_seq" OWNER TO "postgres";
+
+ALTER SEQUENCE "public"."User_id_seq" OWNED BY "public"."User"."id";
+
+ALTER TABLE ONLY "public"."Receipt" ALTER COLUMN "id" SET DEFAULT "nextval"('"public"."Receipt_id_seq"'::"regclass");
+
+ALTER TABLE ONLY "public"."User" ALTER COLUMN "id" SET DEFAULT "nextval"('"public"."User_id_seq"'::"regclass");
+
+ALTER TABLE ONLY "public"."Receipt"
+    ADD CONSTRAINT "Receipt_pkey" PRIMARY KEY ("id");
+
+ALTER TABLE ONLY "public"."User"
+    ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User" USING "btree" ("email");
+
+ALTER TABLE ONLY "public"."Receipt"
+    ADD CONSTRAINT "Receipt_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "public"."User"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+
+GRANT USAGE ON SCHEMA "public" TO "postgres";
+GRANT USAGE ON SCHEMA "public" TO "anon";
+GRANT USAGE ON SCHEMA "public" TO "authenticated";
+GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+GRANT ALL ON TABLE "public"."Receipt" TO "anon";
+GRANT ALL ON TABLE "public"."Receipt" TO "authenticated";
+GRANT ALL ON TABLE "public"."Receipt" TO "service_role";
+
+GRANT ALL ON SEQUENCE "public"."Receipt_id_seq" TO "anon";
+GRANT ALL ON SEQUENCE "public"."Receipt_id_seq" TO "authenticated";
+GRANT ALL ON SEQUENCE "public"."Receipt_id_seq" TO "service_role";
+
+GRANT ALL ON TABLE "public"."User" TO "anon";
+GRANT ALL ON TABLE "public"."User" TO "authenticated";
+GRANT ALL ON TABLE "public"."User" TO "service_role";
+
+GRANT ALL ON SEQUENCE "public"."User_id_seq" TO "anon";
+GRANT ALL ON SEQUENCE "public"."User_id_seq" TO "authenticated";
+GRANT ALL ON SEQUENCE "public"."User_id_seq" TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "service_role";
+
+RESET ALL;
