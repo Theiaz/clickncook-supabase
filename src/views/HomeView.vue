6f1d1e2040fa838137e9aba7add36b32eb2af23d
Julian Schaefer <schaefer@synyx.de>
2023-09-05 18:52:20 +0200

    refactor: delete obsolete components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,46 @@
 <script setup lang="ts">
-import ReceiptForm from '@/components/ReceiptForm.vue'
-</script>
+import { useUser } from '@/composables/useUser'
+import { onMounted, ref } from 'vue'
+import { supabase } from '../supabase'
 
+const name = ref<string>('')
+const description = ref<string>('')
+const loading = ref<boolean>(false)
+
+onMounted(async () => {
+  await getReceipt()
+})
+
+const getReceipt = async () => {
+  try {
+    loading.value = true
+
+    const { user } = useUser()
+
+    const { data, error } = await supabase
+      .from('receipts')
+      .select('name, description')
+      .eq('author_id', user.value!.id)
+      .limit(1)
+      .single()
+
+    if (error) throw error
+
+    if (data) {
+      name.value = data.name
+      description.value = data.description
+    }
+  } catch (error: any) {
+    alert(error.message)
+  } finally {
+    loading.value = false
+  }
+}
+</script>
 <template>
-  <ReceiptForm />
+  <article>
+    <h2>{{ name }}</h2>
+    <p>{{ description }}</p>
+  </article>
+  <router-link :to="{ name: 'newReceipt' }">Create new receipt</router-link>
 </template>
