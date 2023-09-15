133fae543352c368a68eac352afdf5acc1ce16cb
Julian Schaefer <schaefer@synyx.de>
2023-09-15 18:57:46 +0200

    refactor: use generated type from supabase

--- src/types/receipt.ts
+++ src/types/receipt.ts
@@ -1,3 +1,5 @@
+import { supabase } from '@/supabase'
+import type { Database } from '../schema'
 import type { Author } from './author'
 
 interface Receipt {
@@ -8,4 +10,26 @@ interface Receipt {
   authorId: Author['id']
 }
 
-export type { Receipt }
+type ReceiptDAO = Database['public']['Tables']['receipts']['Row']
+
+const mapToDomain = async (data: ReceiptDAO): Promise<Receipt> => {
+  return {
+    id: data.id,
+    name: data.name,
+    description: data.description,
+    authorId: data.author_id!,
+    imgUrl: await mapImageNameToURL(data.img_name)
+  }
+}
+
+const mapImageNameToURL = async (name: string | null) => {
+  if (name === null) {
+    return null
+  }
+
+  const { data } = supabase.storage.from('receipt_images').getPublicUrl(name!)
+
+  return data.publicUrl
+}
+
+export { mapToDomain, type Receipt, type ReceiptDAO }
