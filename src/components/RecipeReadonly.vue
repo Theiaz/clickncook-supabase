96b16f1f6aa5e905f6b32bdffe5f40554cc6cf39
Julian Schaefer <schaefer@synyx.de>
2023-10-20 13:26:13 +0200

    feat: add simple image slideshow

--- src/components/RecipeReadonly.vue
+++ src/components/RecipeReadonly.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
 import type { Recipe } from '@/types/recipe'
+import ImageCarousel from './ImageCarousel.vue'
 
 defineProps<{
   recipe: Recipe
 }>()
-
-// TODO schaefer - duplicated inside image upload
-function toSrc(file: File) {
-  return URL.createObjectURL(file)
-}
 </script>
 <template>
   <h2>{{ recipe.name }}</h2>
-  <template v-for="image in recipe.images" :key="image.name">
-    <img :src="toSrc(image)" :alt="image.name" />
-  </template>
-
+  <ImageCarousel :images="recipe.images" />
   <div>{{ recipe.description }}</div>
 </template>
