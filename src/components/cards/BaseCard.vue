5a8b24b420efd1449cda2a95d924344f1ba07dc6
Julian Schaefer <schaefer@synyx.de>
2023-11-03 14:37:48 +0100

    style: improve cards

---
+++ src/components/cards/BaseCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <article class="border-transparent rounded-2xl shadow-md shadow-primary/30">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <section class="p-4">
+      <slot name="content"></slot>
+    </section>
+  </article>
+</template>
---
+++ src/components/cards/RecipeCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import ImageCarousel from '@/components/ImageCarousel.vue'
+import BaseCard from '@/components/cards/BaseCard.vue'
+import RecipeHeader from '@/components/recipes/RecipeHeader.vue'
+import type { Recipe } from '@/types/recipe'
+
+const props = defineProps<{
+  recipe: Recipe
+}>()
+</script>
+<template>
+  <BaseCard>
+    <template #header>
+      <ImageCarousel :images="recipe!.images" />
+    </template>
+    <template #content>
+      <RecipeHeader :recipe="recipe" />
+      <p>{{ props.recipe.description }}</p>
+    </template>
+  </BaseCard>
+</template>
