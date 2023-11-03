23d3e710e8335ddd359bc5e286aacc8402121ca8
Julian Schaefer <schaefer@synyx.de>
2023-11-03 09:19:31 +0100

    style: add buttons on homeview

---
+++ src/components/buttons/PrimaryButton.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import BaseButton from '@/components/buttons/BaseButton.vue'
+</script>
+<template>
+  <BaseButton class="bg-primary">
+    <slot></slot>
+  </BaseButton>
+</template>
---
+++ src/components/buttons/SecondaryButton.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import BaseButton from '@/components/buttons/BaseButton.vue'
+</script>
+<template>
+  <BaseButton class="bg-secondary">
+    <slot></slot>
+  </BaseButton>
+</template>
