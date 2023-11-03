234e6c20ccca49a8961bd32336a540fa56332120
Julian Schaefer <schaefer@synyx.de>
2023-11-03 12:07:02 +0100

    style: add recipe header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { findRandomRecipe } from '@/api/recipes/api'
-import RecipeCard from '@/components/RecipeCard.vue'
+import ImageCarousel from '@/components/ImageCarousel.vue'
 import PrimaryButton from '@/components/buttons/PrimaryButton.vue'
+import RecipeHeader from '@/components/recipes/RecipeHeader.vue'
 import BottomButtonLayout from '@/layouts/BottomButtonLayout.vue'
 import { useCurrentRecipeStore } from '@/stores/currentRecipe'
 import { storeToRefs } from 'pinia'
@@ -37,16 +38,24 @@ const getRandomRecipe = async () => {
 <template>
   <BottomButtonLayout>
     <template #content>
-      <RecipeCard v-if="hasRecipe" :recipe="recipe" />
+      <template v-if="hasRecipe">
+        <ImageCarousel class="full-width" :images="recipe!.images" />
+        <RecipeHeader :recipe="recipe" />
+      </template>
       <p v-else>There are no recipes. Start creating one!</p>
     </template>
     <template #actions v-if="hasRecipe">
       <router-link
         :to="{ name: 'details', params: { id: recipe.id } }"
-        class="rounded-full px-4 text-white bg-secondary"
+        class="rounded-full px-4 py-2 text-white bg-secondary"
         >Show Details
       </router-link>
       <PrimaryButton @click="getRandomRecipe">New Recipe</PrimaryButton>
     </template>
   </BottomButtonLayout>
 </template>
+<style scoped>
+.full-width {
+  @apply relative left-1/2 right-1/2 w-screen max-w-[100vw] -mx-[50vw];
+}
+</style>
