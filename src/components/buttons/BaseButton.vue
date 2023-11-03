12ad5748293576f001e20abe9ccb2d211ec05813
Julian Schaefer <schaefer@synyx.de>
2023-11-03 12:04:18 +0100

    style: add apdding to button

--- src/components/buttons/BaseButton.vue
+++ src/components/buttons/BaseButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="rounded-full px-4 text-white hover:cursor-pointer">
+  <button class="rounded-full px-4 py-2 text-white hover:cursor-pointer">
     <slot></slot>
   </button>
 </template>
