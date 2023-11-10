097e662115dc01bb0183d945cc14a6004c6daa69
Julian Schaefer <schaefer@synyx.de>
2023-11-10 13:11:11 +0100

    refactor: move action bar beneath images

--- src/components/recipes/RecipeDetails.vue
+++ src/components/recipes/RecipeDetails.vue
@@ -1,44 +1,14 @@
 <script setup lang="ts">
-import { useUser } from '@/composables/useUser'
-import router from '@/router'
-import { useCurrentRecipeStore } from '@/stores/currentRecipe'
 import type { Recipe } from '@/types/recipe'
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
 import ImageCarousel from '../ImageCarousel.vue'
-import EditIcon from '../icons/EditIcon.vue'
-import TrashIcon from '../icons/TrashIcon.vue'
 import RecipeHeader from './RecipeHeader.vue'
 
-const props = defineProps<{
+defineProps<{
   recipe: Recipe
 }>()
-const recipeStore = useCurrentRecipeStore()
-
-const { user } = useUser()
-const isOwnRecipe = computed(() => props.recipe.authorId === user.value?.id)
-const route = useRoute()
-const isHomeScreen = computed(() => route.name === 'home')
-const shouldDisplayActions = computed(() => isOwnRecipe.value && !isHomeScreen.value)
-
-const onDelete = async () => {
-  if (props.recipe) {
-    await recipeStore.deleteRecipe(props.recipe)
-    await router.push({ name: 'home' })
-  }
-}
 </script>
 <template>
-  <ImageCarousel class="full-width" :images="recipe!.images">
-    <template #actions v-if="shouldDisplayActions">
-      <router-link :to="{ name: 'edit', params: { id: recipe.id } }">
-        <EditIcon />
-      </router-link>
-      <button @click="onDelete">
-        <TrashIcon />
-      </button>
-    </template>
-  </ImageCarousel>
+  <ImageCarousel class="full-width" :images="recipe!.images" />
   <section class="mt-2 flex flex-col gap-4">
     <RecipeHeader :recipe="recipe" />
     <div>{{ recipe.description }}</div>
--- src/components/recipes/RecipeHeader.vue
+++ src/components/recipes/RecipeHeader.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
+import { useUser } from '@/composables/useUser'
+import router from '@/router'
+import { useCurrentRecipeStore } from '@/stores/currentRecipe'
 import type { Recipe } from '@/types/recipe'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import EditIcon from '../icons/EditIcon.vue'
+import TrashIcon from '../icons/TrashIcon.vue'
 
-defineProps<{
+const props = defineProps<{
   recipe: Recipe
 }>()
+
+const recipeStore = useCurrentRecipeStore()
+
+const { user } = useUser()
+const isOwnRecipe = computed(() => props.recipe.authorId === user.value?.id)
+const route = useRoute()
+const isHomeScreen = computed(() => route.name === 'home')
+const shouldDisplayActions = computed(() => isOwnRecipe.value && !isHomeScreen.value)
+
+const onDelete = async () => {
+  if (props.recipe) {
+    await recipeStore.deleteRecipe(props.recipe)
+    await router.push({ name: 'home' })
+  }
+}
 </script>
 <template>
-  <h2 class="text-primary font-bold">{{ recipe.name }}</h2>
+  <div class="flex justify-between  text-primary">
+    <h2 class="font-bold">{{ recipe.name }}</h2>
+    <div v-if="shouldDisplayActions" class="flex gap-4">
+      <router-link :to="{ name: 'edit', params: { id: recipe.id } }">
+        <EditIcon />
+      </router-link>
+      <button @click="onDelete">
+        <TrashIcon />
+      </button>
+    </div>
+  </div>
 </template>
