3facb4750f76ae06fc460090485cf8f6235c59ce
Julian Schaefer <schaefer@synyx.de>
2023-11-24 13:02:27 +0100

    refactor: remove unused type

--- src/types/recipe.ts
+++ src/types/recipe.ts
@@ -1,12 +1,3 @@
-type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
-  ? Acc[number]
-  : Enumerate<N, [...Acc, Acc['length']]>
-
-type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>
-
-type Rating = IntRange<0, 5>
-
-
 type RecipeData = {
   name: string | null
   description?: string
