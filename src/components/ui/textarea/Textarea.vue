2d7cc174efa2235485be646961657d13536dccb6
Julian Schaefer <schaefer@synyx.de>
2024-01-19 16:52:58 +0100

    chore: format

--- src/components/ui/textarea/Textarea.vue
+++ src/components/ui/textarea/Textarea.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { useVModel } from '@vueuse/core'
 import { cn } from '@/util/shadcn'
+import { useVModel } from '@vueuse/core'
 
 const props = defineProps<{
   defaultValue?: string | number
@@ -13,10 +13,18 @@ const emits = defineEmits<{
 
 const modelValue = useVModel(props, 'modelValue', emits, {
   passive: true,
-  defaultValue: props.defaultValue,
+  defaultValue: props.defaultValue
 })
 </script>
 
 <template>
-  <textarea v-model="modelValue" :class="cn('flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', $attrs.class ?? '')" />
+  <textarea
+    v-model="modelValue"
+    :class="
+      cn(
+        'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        $attrs.class ?? ''
+      )
+    "
+  />
 </template>
