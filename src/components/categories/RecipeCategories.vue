d3333056915af9dad529cfdd208dcd07fc0a7305
Julian Schaefer <schaefer@synyx.de>
2024-02-09 09:40:20 +0100

    feat: display category editor

--- src/components/categories/RecipeCategories.vue
+++ src/components/categories/RecipeCategories.vue
@@ -1,21 +1,73 @@
 <script setup lang="ts">
 import { Badge } from '@/components/ui/badge'
 import type { Category } from '@/types/category'
+import EditIcon from '@/components/icons/EditIcon.vue'
 import { getCategories } from '@/api/categories/api'
-import { onBeforeMount, ref } from 'vue'
+import { onBeforeMount, ref, watch } from 'vue'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 
+const props = withDefaults(
+  defineProps<{
+    modelValue: Category[]
+    isReadonly?: boolean
+  }>(),
+  {
+    isReadonly: true
+  }
+)
+
+const emit = defineEmits<{
+  'update:modelValue': [modelValue: Category[]]
+}>()
+
+// available categories
+const availableCategories = ref<Category[]>([])
 onBeforeMount(async () => {
-  categories.value = await getCategories()
+  availableCategories.value = await getCategories()
 })
 
-const categories = ref<Category[]>([])
+// selection
+const selectedCategories = ref<Category[]>(props.modelValue)
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    selectedCategories.value = newValue
+  }
+)
 
+const toggleSelection = (selectedCategory: Category) => {
+  if (selectedCategories.value.includes(selectedCategory)) {
+    selectedCategories.value = selectedCategories.value.filter((c) => c !== selectedCategory)
+  } else {
+    selectedCategories.value.push(selectedCategory)
+  }
+  emit('update:modelValue', selectedCategories.value)
+}
 </script>
 <template>
-  <div class="flex gap-4 overflow-x-scroll snap-x snap-mandatory no-scrollbar">
-    <Badge v-for="category in categories" :key="category.id" class="snap-center">
-      {{ category.name }}
-    </Badge>
+  <label class="block text-sm font-medium">Categories</label>
+  <div class="flex gap-4">
+    <Popover v-if="!isReadonly">
+      <PopoverTrigger>
+        <EditIcon />
+      </PopoverTrigger>
+      <PopoverContent class="flex gap-4 flex-wrap mx-8 my-2">
+        <Badge
+          v-for="category in availableCategories"
+          :key="category.id"
+          class="snap-center cursor-pointer"
+          :variant="selectedCategories.includes(category) ? 'default' : 'outline'"
+          @click="toggleSelection(category)"
+        >
+          {{ category.name }}
+        </Badge>
+      </PopoverContent>
+    </Popover>
+    <div class="w-full flex gap-4 overflow-x-scroll snap-x snap-mandatory no-scrollbar">
+      <Badge v-for="category in selectedCategories" :key="category.id" class="snap-center">
+        {{ category.name }}
+      </Badge>
+    </div>
   </div>
 </template>
 <style scoped>
