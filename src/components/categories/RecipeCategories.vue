d59c38f06d62c751d205843190bab7872687be1c
Julian Schaefer <schaefer@synyx.de>
2024-02-09 08:46:51 +0100

    refactor: move recipe code into component

--- src/components/categories/RecipeCategories.vue
+++ src/components/categories/RecipeCategories.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import { Badge } from '@/components/ui/badge'
 import type { Category } from '@/types/category'
+import { getCategories } from '@/api/categories/api'
+import { onBeforeMount, ref } from 'vue'
 
-const props = defineProps<{
-  categories: Category[]
-}>()
+onBeforeMount(async () => {
+  categories.value = await getCategories()
+})
+
+const categories = ref<Category[]>([])
+
 </script>
 <template>
   <div class="flex gap-4 overflow-x-scroll snap-x snap-mandatory no-scrollbar">
-    <Badge v-for="category in props.categories" :key="category.id" class="snap-center">
+    <Badge v-for="category in categories" :key="category.id" class="snap-center">
       {{ category.name }}
     </Badge>
   </div>
