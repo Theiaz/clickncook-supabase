7264d6002e055ce793b5f3067caede1c1aae6bad
Julian Schaefer <schaefer@synyx.de>
2024-02-23 12:11:58 +0100

    feat: seed categories for local startup

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,3 +1,9 @@
 INSERT INTO auth.users (instance_id, id, aud, role, email, encrypted_password, email_confirmed_at, invited_at, confirmation_token, confirmation_sent_at, recovery_token, recovery_sent_at, email_change_token_new, email_change, email_change_sent_at, last_sign_in_at, raw_app_meta_data, raw_user_meta_data, is_super_admin, created_at, updated_at, phone, phone_confirmed_at, phone_change, phone_change_token, phone_change_sent_at, email_change_token_current, email_change_confirm_status, banned_until, reauthentication_token, reauthentication_sent_at, is_sso_user, deleted_at) VALUES ('00000000-0000-0000-0000-000000000000', '7c0ffe18-5cff-4519-aa2b-9a82c065ceb9', 'authenticated', 'authenticated', 'foo@foo.de', '$2a$10$XzQqwPTGAW9UH9lGJ5PYkeAMaBYSt44/vk1a5klxqdyjGieSF8EJC', '2023-09-03 13:14:57.51121+00', NULL, '', NULL, '', NULL, '', '', NULL, '2023-09-03 13:14:57.512836+00', '{"provider": "email", "providers": ["email"]}', '{}', NULL, '2023-09-03 13:14:57.506079+00', '2023-09-03 13:14:57.514448+00', NULL, NULL, '', '', NULL, '', 0, NULL, '', NULL, false, NULL);
 
 INSERT INTO auth.identities (provider_id, user_id, identity_data, provider, last_sign_in_at, created_at, updated_at) VALUES ('7c0ffe18-5cff-4519-aa2b-9a82c065ceb9', '7c0ffe18-5cff-4519-aa2b-9a82c065ceb9', '{"sub": "7c0ffe18-5cff-4519-aa2b-9a82c065ceb9", "email": "foo@foo.de"}', 'email', '2023-09-03 13:14:57.510079+00', '2023-09-03 13:14:57.510096+00', '2023-09-03 13:14:57.510096+00');
+
+-- sample categories
+INSERT INTO public.categories (id, name) VALUES ('6e681e14-4005-49e1-b4e2-99f27b9a18de', 'Vegetarian');
+INSERT INTO public.categories (id, name) VALUES ('3a050460-b989-4f68-81e8-12df5bda1f0a', 'Dessert');
+INSERT INTO public.categories (id, name) VALUES ('b2ad8bda-16bc-4cb4-9bd3-e1b51b861a8f', 'Fast');
+INSERT INTO public.categories (id, name) VALUES ('b2ad8bda-16bc-4cb4-9bd3-e1b51b861a89', 'Healthy');
