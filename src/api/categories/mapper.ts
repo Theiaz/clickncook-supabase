7381760eda4f814856cbd53301a31866b8bcd627
Julian Schaefer <schaefer@synyx.de>
2024-03-15 14:13:46 +0100

    fix: linting errors

--- src/api/categories/mapper.ts
+++ src/api/categories/mapper.ts
@@ -4,7 +4,7 @@ import type { CategoryDto } from './dto'
 const mapToDomain = async (data: CategoryDto): Promise<Category> => {
   return {
     id: data.id,
-    name: data.name,
+    name: data.name
   }
 }
 
