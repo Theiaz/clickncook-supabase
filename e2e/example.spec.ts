5ae96d788ac0155dcffe33efde166cea27647ec6
Julian Schaefer <schaefer@synyx.de>
2024-03-26 20:00:13 +0100

    test: add test case for login and recipe creation

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -4,3 +4,21 @@ test('visits the app root url', async ({ page }) => {
   await page.goto('/')
   await expect(page.locator('[data-test="claim"]')).toBeVisible()
 })
+
+test('login and create recipe', async ({ page }) => {
+  await page.goto('/')
+  // create button
+  await page.getByRole('button').first().click()
+
+  //login
+  await page.getByLabel('Email').fill('foo@foo.de')
+  await page.getByLabel('Email').press('Tab')
+  await page.getByLabel('Password').fill('123456')
+  page.once('dialog', (dialog) => {
+    console.log(`Dialog message: ${dialog.message()}`)
+    dialog.dismiss().catch(() => {})
+  })
+  await page.getByRole('button', { name: 'Login' }).click()
+
+  await expect(page.getByRole('button', { name: 'Create Recipe' })).toBeVisible()
+})
