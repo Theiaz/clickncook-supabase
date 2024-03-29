ac3b23858e1ff578163d40fae120e052144d203c
Julian Schaefer <schaefer@synyx.de>
2024-03-29 14:22:08 +0100

    test: add e2e tests for auth

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -21,14 +21,18 @@ const submitText = computed<string>(() => {
     <form @submit.prevent="login(email, password)" class="flex flex-col gap-4">
       <div class="grid w-full max-w-sm items-center gap-1.5">
         <Label for="email">Email</Label>
-        <Input id="email" type="email" v-model="email" v-focus />
+        <Input id="email" type="email" v-model="email" v-focus data-test="mail-input" />
       </div>
       <div class="grid w-full max-w-sm items-center gap-1.5">
         <Label for="password">Password</Label>
-        <Input id="password" type="password" v-model="password" />
+        <Input id="password" type="password" v-model="password" data-test="password-input" />
       </div>
       <div class="flex gap-4">
-        <router-link :to="{ name: 'register' }" v-slot="{ href, navigate }">
+        <router-link
+          :to="{ name: 'register' }"
+          v-slot="{ href, navigate }"
+          data-test="register-button"
+        >
           <Button variant="secondary" :href="href" @click="navigate">Register</Button>
         </router-link>
         <Button>{{ submitText }}</Button>
