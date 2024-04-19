ec367d2e3e33d52f39e1260cfd05d407a4a1c478
Julian Schaefer <schaefer@synyx.de>
2024-04-19 14:25:06 +0200

    ci: fix typecheck failure

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,12 +1,7 @@
-import { SupabaseClient, createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 import { type Database } from './types/schema'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL ?? ''
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY ?? ''
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-let supabase: SupabaseClient | null = null
-if (supabaseUrl && supabaseAnonKey) {
-  supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
-}
-
-export { supabase }
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
