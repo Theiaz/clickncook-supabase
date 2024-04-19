b734b074a79cb6fd77f1d4342b8c456b872e7675
Julian Schaefer <schaefer@synyx.de>
2024-04-19 20:46:31 +0200

    feature: disable image compression by feature flag

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+type Config = { enableImageCompression: boolean }
+
+// TODO schaefer - we need to use the server for feature flags
+const config: Config = { enableImageCompression: false }
+
+export { config }
