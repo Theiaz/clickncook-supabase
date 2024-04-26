438d00960b609f3f24e77437a36f3879edd71b4a
Julian Schaefer <schaefer@synyx.de>
2024-04-26 11:54:31 +0200

    feat: shrink images to fixed size

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 type Config = { enableImageCompression: boolean }
 
 // TODO schaefer - we need to use the server for feature flags
-const config: Config = { enableImageCompression: false }
+const config: Config = { enableImageCompression: true }
 
 export { config }
