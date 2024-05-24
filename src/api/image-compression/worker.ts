c4dabd8cafc138cc805697d744722d15922d6947
Julian Schaefer <schaefer@synyx.de>
2024-05-24 18:44:33 +0200

    refactor: drop photon and use native compression

--- src/api/image-compression/worker.ts
+++ src/api/image-compression/worker.ts
@@ -1,28 +1,17 @@
-import type { PhotonImage } from '@silvia-odwyer/photon'
-
-const loadWasm = async () => {
-  return await import('@silvia-odwyer/photon')
-}
+const IMAGE_TYPE = 'image/webp'
 
 onmessage = async (msg: MessageEvent<File>) => {
-  const photon = await loadWasm()
+  const img: File = msg.data
+  const imageBitmap = await createImageBitmap(img)
 
-  const image: File = msg.data
-  const imageBitmap: ImageBitmap = await createImageBitmap(image)
-  const width = imageBitmap.width
-  const height = imageBitmap.height
-
-  const offscreenCanvas = new OffscreenCanvas(width, height)
+  const offscreenCanvas = new OffscreenCanvas(imageBitmap.width / 2, imageBitmap.height / 2)
   const ctx = offscreenCanvas.getContext('2d')
   if (ctx == null) return
+
+  // Draw the ImageBitmap onto the OffscreenCanvas, resizing it
   ctx.drawImage(imageBitmap, 0, 0, offscreenCanvas.width, offscreenCanvas.height)
+  const blob = await offscreenCanvas.convertToBlob({ type: IMAGE_TYPE })
+  const resizedImg = new File([blob], img.name, { type: IMAGE_TYPE })
 
-  // Convert the ImageData found in the canvas to a PhotonImage (so that it can communicate with the core Rust library)
-  // @ts-ignore
-  const photonImage: PhotonImage = photon.open_image(offscreenCanvas, ctx)
-  const resizedPhotonImage: PhotonImage = photon.resize(photonImage, width / 2, height / 2, 1)
-
-  const blob = new Blob([resizedPhotonImage.get_bytes()])
-  const compressedImage = new File([blob], image.name, { type: image.type })
-  postMessage(compressedImage)
+  postMessage(resizedImg)
 }
