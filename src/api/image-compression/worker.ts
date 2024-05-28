d32341dbb3ff7f99da50424dffa08e8613bcdccd
Julian Schaefer <schaefer@synyx.de>
2024-05-28 20:27:30 +0200

    wip: change to compressorjs

--- src/api/image-compression/worker.ts
+++ src/api/image-compression/worker.ts
@@ -1,28 +1,22 @@
-import type { PhotonImage } from '@silvia-odwyer/photon'
-
 const loadWasm = async () => {
-  return await import('@silvia-odwyer/photon')
+  const compressor = await import('compressorjs')
+  return compressor
 }
 
 onmessage = async (msg: MessageEvent<File>) => {
-  const photon = await loadWasm()
-
   const image: File = msg.data
-  const imageBitmap: ImageBitmap = await createImageBitmap(image)
-  const width = imageBitmap.width
-  const height = imageBitmap.height
+  const compressor = await loadWasm()
+  new compressor.default(image, {
+    quality: 0.6,
 
-  const offscreenCanvas = new OffscreenCanvas(width, height)
-  const ctx = offscreenCanvas.getContext('2d')
-  if (ctx == null) return
-  ctx.drawImage(imageBitmap, 0, 0, offscreenCanvas.width, offscreenCanvas.height)
-
-  // Convert the ImageData found in the canvas to a PhotonImage (so that it can communicate with the core Rust library)
-  // @ts-ignore
-  const photonImage: PhotonImage = photon.open_image(offscreenCanvas, ctx)
-  const resizedPhotonImage: PhotonImage = photon.resize(photonImage, width / 2, height / 2, 1)
-
-  const blob = new Blob([resizedPhotonImage.get_bytes()])
-  const compressedImage = new File([blob], image.name, { type: image.type })
-  postMessage(compressedImage)
+    // The compression process is asynchronous,
+    // which means you have to access the `result` in the `success` hook function.
+    success(result) {
+      console.log('Compressed image size: ', result.size)
+      postMessage(result)
+    },
+    error(err) {
+      console.log(err.message)
+    }
+  })
 }
