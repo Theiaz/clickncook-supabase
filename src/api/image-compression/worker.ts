86e268a539977ac4c2c3810d2b3a1fe927b517d0
Julian Schäfer <Theiaz@users.noreply.github.com>
2024-05-28 20:59:07 +0200

    Switch image compression (#34)
    perf: change compression lib to browser-image-compression

--- src/api/image-compression/worker.ts
+++ src/api/image-compression/worker.ts
@@ -1,17 +1,21 @@
-const IMAGE_TYPE = 'image/webp'
+const loadCompressionLib = async () => {
+  const { default: imageCompression } = await import('browser-image-compression')
+  return imageCompression
+}
 
 onmessage = async (msg: MessageEvent<File>) => {
-  const img: File = msg.data
-  const imageBitmap = await createImageBitmap(img)
+  const image: File = msg.data
+  const imageCompression = await loadCompressionLib()
 
-  const offscreenCanvas = new OffscreenCanvas(imageBitmap.width / 2, imageBitmap.height / 2)
-  const ctx = offscreenCanvas.getContext('2d')
-  if (ctx == null) return
-
-  // Draw the ImageBitmap onto the OffscreenCanvas, resizing it
-  ctx.drawImage(imageBitmap, 0, 0, offscreenCanvas.width, offscreenCanvas.height)
-  const blob = await offscreenCanvas.convertToBlob({ type: IMAGE_TYPE })
-  const resizedImg = new File([blob], img.name, { type: IMAGE_TYPE })
-
-  postMessage(resizedImg)
+  const options = {
+    maxSizeMB: 1,
+    maxWidthOrHeight: 1920,
+    useWebWorker: false
+  }
+  try {
+    const compressedFile = await imageCompression(image, options)
+    postMessage(compressedFile)
+  } catch (error) {
+    console.log(error)
+  }
 }
