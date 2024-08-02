9742a670e18d3e3b1ccea00e7e6b2ae8aa717089
Julian Schaefer <schaefer@synyx.de>
2024-08-02 14:44:31 +0200

    build: only use sentry for production build

--- vite.config.ts
+++ vite.config.ts
@@ -4,94 +4,103 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: "Click 'n' Cook",
-        short_name: "C'n'C",
-        description: 'Get random recipes for cooking!',
-        theme_color: '#00473E',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/pwa-maskable-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable'
-          },
-          {
-            src: '/pwa-maskable-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ],
-        shortcuts: [
-          {
-            name: 'Create a new recipe',
-            short_name: 'Create',
-            icons: [
-              {
-                src: '/shortcut-new.png',
-                sizes: '192x192',
-                type: 'image/png',
-                purpose: 'any'
-              }
-            ],
-            description: 'Create a new recipe',
-            url: '/new'
-          },
-          {
-            name: 'View my recipes list',
-            short_name: 'My recipes',
-            icons: [
-              {
-                src: '/shortcut-my.png',
-                sizes: '192x192',
-                type: 'image/png',
-                purpose: 'any'
-              }
-            ],
-            description: 'View your saved recipes',
-            url: '/my'
-          }
-        ]
-      },
-      devOptions: {
-        enabled: false // enable sw on development
-      }
-    }),
-    sentryVitePlugin({
-      org: 'julian-schaefer',
-      project: 'clickncook-supabase'
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+const plugins = [
+  vue(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+      name: "Click 'n' Cook",
+      short_name: "C'n'C",
+      description: 'Get random recipes for cooking!',
+      theme_color: '#00473E',
+      icons: [
+        {
+          src: '/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: '/pwa-maskable-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable'
+        },
+        {
+          src: '/pwa-maskable-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ],
+      shortcuts: [
+        {
+          name: 'Create a new recipe',
+          short_name: 'Create',
+          icons: [
+            {
+              src: '/shortcut-new.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'any'
+            }
+          ],
+          description: 'Create a new recipe',
+          url: '/new'
+        },
+        {
+          name: 'View my recipes list',
+          short_name: 'My recipes',
+          icons: [
+            {
+              src: '/shortcut-my.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'any'
+            }
+          ],
+          description: 'View your saved recipes',
+          url: '/my'
+        }
+      ]
+    },
+    devOptions: {
+      enabled: false // enable sw on development
     }
-  },
-  build: {
-    target: 'es2022',
-    sourcemap: true
-  },
-  worker: {
-    format: 'es'
+  })
+]
+
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build'
+  if (isProduction) {
+    plugins.push(
+      sentryVitePlugin({
+        org: 'julian-schafer-b1',
+        project: 'clickncook-supabase'
+      })
+    )
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      target: 'es2022',
+      sourcemap: true
+    },
+    worker: {
+      format: 'es'
+    }
   }
 })
